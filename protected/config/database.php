751723f162cacfa4250b4b87f2125323ab95d951
mukhlish32@ <mukhlish.syarif@gmail.com>
2025-01-10 10:20:39 +0800

    setting: database config

--- protected/config/database.php
+++ protected/config/database.php
@@ -2,7 +2,7 @@
 
 // This is the database connection configuration.
 return array(
-	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+	// 'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
 	/*
 	'connectionString' => 'mysql:host=localhost;dbname=testdrive',
@@ -11,4 +11,11 @@ return array(
 	'password' => '',
 	'charset' => 'utf8',
 	*/
+
+	'connectionString' => 'pgsql:host=' . getenv('DB_HOST') .
+		';port=' . getenv('DB_PORT') .
+		';dbname=' . getenv('DB_NAME'),
+	'username' => getenv('DB_USER'),
+	'password' => getenv('DB_PASSWORD'),
+
 );
