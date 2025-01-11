2191470ac9aa53366fde9e8d0a2b88a5a9ea89e6
mukhlish32@ <mukhlish.syarif@gmail.com>
2025-01-11 22:52:28 +0800

    update: fix function

--- protected/commands/WilayahSeederCommand.php
+++ protected/commands/WilayahSeederCommand.php
@@ -11,13 +11,20 @@ class WilayahSeederCommand extends CConsoleCommand
     public function seedWilayah()
     {
         $sqlFilePath = Yii::getPathOfAlias('application.data') . '/wilayah.sql';
-        
+
         if (file_exists($sqlFilePath)) {
             $sql = file_get_contents($sqlFilePath);
+            $queries = explode(";", $sql);
+
             $connection = Yii::app()->db;
-            
+
             try {
-                $connection->createCommand($sql)->execute();
+                foreach ($queries as $query) {
+                    $query = trim($query);
+                    if ($query) {
+                        $connection->createCommand($query)->execute();
+                    }
+                }
                 echo "Seed data from wilayah.sql successfully executed.\n";
             } catch (Exception $e) {
                 echo "Error executing the SQL file: " . $e->getMessage() . "\n";
