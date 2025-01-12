be9bb30505313446085129790c4d53f638ffe226
mukhlish32@ <mukhlish.syarif@gmail.com>
2025-01-12 22:45:15 +0800

    new: setting access of button in views

--- protected/views/master/role/_actions.php
+++ protected/views/master/role/_actions.php
@@ -1,14 +1,32 @@
-<?php
-/* @var $this RoleController */
-/* @var $role Role */
-?>
-
-<div class="action-buttons">
-    <a href="<?php echo Yii::app()->createUrl('master/role/view', ['id' => $role->id]); ?>" class="btn btn-info btn-sm">View</a>
-    <a href="<?php echo Yii::app()->createUrl('master/role/update', ['id' => $role->id]); ?>" class="btn btn-warning btn-sm">Edit</a>
-    
-    <!-- Delete form -->
-    <?php echo CHtml::beginForm(Yii::app()->createUrl('master/role/delete', ['id' => $role->id]), 'post', ['style' => 'display:inline']); ?>
-        <button type="submit" class="btn btn-danger btn-sm delete-button" onclick="return confirm('Are you sure you want to delete this item?');">Delete</button>
-    <?php echo CHtml::endForm(); ?>
+<div class="action-buttons d-flex flex-column gap-2">
+    <div class="d-flex gap-2">
+        <?php if ($this->checkAccess($location, 'permission')): ?>
+            <a href="<?php echo Yii::app()->createUrl($location . '/permissions', ['id' => $model->id]); ?>" class="btn btn-sm btn-success btn-label w-md">
+                <i class="ri-list-check label-icon align-middle me-1"></i>
+                <span>Hak Akses</span>
+            </a>
+        <?php endif; ?>
+        <?php if ($this->checkAccess($location, 'view')): ?>
+            <a href="<?php echo Yii::app()->createUrl($location . '/view', ['id' => $model->id]); ?>" class="btn btn-sm btn-info btn-label w-md">
+                <i class="ri-eye-line label-icon align-middle me-1"></i>
+                <span>Detail</span>
+            </a>
+        <?php endif; ?>
+    </div>
+    <div class="d-flex gap-2">
+        <?php if ($this->checkAccess($location, 'update')): ?>
+            <a href="<?php echo Yii::app()->createUrl($location . '/update', ['id' => $model->id]); ?>" class="btn btn-sm btn-warning btn-label w-md">
+                <i class="ri-edit-line label-icon align-middle me-1"></i>
+                <span>Edit</span>
+            </a>
+        <?php endif; ?>
+        <?php if ($this->checkAccess($location, 'delete')): ?>
+            <?php echo CHtml::beginForm(Yii::app()->createUrl($location . '/delete', ['id' => $model->id]), 'post', ['style' => 'display:inline']); ?>
+            <button type="submit" class="btn btn-sm btn-danger btn-label w-md delete-button">
+                <i class="ri-delete-bin-line label-icon align-middle me-1"></i>
+                <span>Hapus</span>
+            </button>
+        <?php endif; ?>
+        <?php echo CHtml::endForm(); ?>
+    </div>
 </div>
--- protected/views/transaksi/pasien/_actions.php
+++ protected/views/transaksi/pasien/_actions.php
@@ -1,24 +1,32 @@
 <div class="action-buttons d-flex flex-column gap-2">
     <div class="d-flex gap-2">
-        <a href="<?php echo Yii::app()->createUrl($location . '/daftar', ['id' => $model->id]); ?>" class="btn btn-sm btn-success btn-label w-md">
-            <i class="ri-list-check label-icon align-middle me-1"></i>
-            <span>Daftar</span>
-        </a>
-        <a href="<?php echo Yii::app()->createUrl($location . '/view', ['id' => $model->id]); ?>" class="btn btn-sm btn-info btn-label w-md">
-            <i class="ri-eye-line label-icon align-middle me-1"></i>
-            <span>Detail</span>
-        </a>
+        <?php if ($this->checkAccess($location, 'register')): ?>
+            <a href="<?php echo Yii::app()->createUrl($location . '/daftar', ['id' => $model->id]); ?>" class="btn btn-sm btn-success btn-label w-md">
+                <i class="ri-list-check label-icon align-middle me-1"></i>
+                <span>Daftar</span>
+            </a>
+        <?php endif; ?>
+        <?php if ($this->checkAccess($location, 'view')): ?>
+            <a href="<?php echo Yii::app()->createUrl($location . '/view', ['id' => $model->id]); ?>" class="btn btn-sm btn-info btn-label w-md">
+                <i class="ri-eye-line label-icon align-middle me-1"></i>
+                <span>Detail</span>
+            </a>
+        <?php endif; ?>
     </div>
     <div class="d-flex gap-2">
-        <a href="<?php echo Yii::app()->createUrl($location . '/update', ['id' => $model->id]); ?>" class="btn btn-sm btn-warning btn-label w-md">
-            <i class="ri-edit-line label-icon align-middle me-1"></i>
-            <span>Edit</span>
-        </a>
-        <?php echo CHtml::beginForm(Yii::app()->createUrl($location . '/delete', ['id' => $model->id]), 'post', ['style' => 'display:inline']); ?>
-        <button type="submit" class="btn btn-sm btn-danger btn-label w-md delete-button">
-            <i class="ri-delete-bin-line label-icon align-middle me-1"></i>
-            <span>Hapus</span>
-        </button>
+        <?php if ($this->checkAccess($location, 'update')): ?>
+            <a href="<?php echo Yii::app()->createUrl($location . '/update', ['id' => $model->id]); ?>" class="btn btn-sm btn-warning btn-label w-md">
+                <i class="ri-edit-line label-icon align-middle me-1"></i>
+                <span>Edit</span>
+            </a>
+        <?php endif; ?>
+        <?php if ($this->checkAccess($location, 'delete')): ?>
+            <?php echo CHtml::beginForm(Yii::app()->createUrl($location . '/delete', ['id' => $model->id]), 'post', ['style' => 'display:inline']); ?>
+            <button type="submit" class="btn btn-sm btn-danger btn-label w-md delete-button">
+                <i class="ri-delete-bin-line label-icon align-middle me-1"></i>
+                <span>Hapus</span>
+            </button>
+        <?php endif; ?>
         <?php echo CHtml::endForm(); ?>
     </div>
 </div>
